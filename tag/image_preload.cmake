09568822f7c0622b7c0b9e61f9b9f98a81c31999
NickTheWhale <loehrketurkey7@gmail.com>
2024-02-14 16:44:06 -0600

    updated gitignore

--- tag/image_preload.cmake
+++
@@ -1,8 +0,0 @@
-# Generated file that can be used to preload variant images
-set(APP_MODE "TAG" CACHE INTERNAL "NCS child image controlled")
-set(BOARD "decawave_dwm1001_dev" CACHE INTERNAL "NCS child image controlled")
-set(CACHED_CONF_FILE "c:/Users/nicholas/repos/tdoa/application/prj.conf" CACHE INTERNAL "NCS child image controlled")
-set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE "" CACHE INTERNAL "NCS child image controlled")
-set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
-set(WEST_PYTHON "C:/ncs/toolchains/c57af46cb7/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
