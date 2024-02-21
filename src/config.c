a8bea1469b626f38a0c9e87971cd5707501cea29
NickTheWhale <loehrketurkey7@gmail.com>
2024-02-20 23:11:31 -0600

    flash storage

--- src/config.c
+++ src/config.c
@@ -1,7 +1,7 @@
 #include "config.h"
 
-#include <stdint.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/flash.h>
 #include <zephyr/fs/nvs.h>
@@ -16,22 +16,37 @@ LOG_MODULE_REGISTER(config, LOG_LEVEL_DBG);
 #define NVS_PARTITION_OFFSET FIXED_PARTITION_OFFSET(NVS_PARTITION)
 
 #define CONFIG_ID 0
-#define READ_LENGTH 100
+#define MAGIC 0xDECA
+#define VERSION_MAJOR 1
+#define VERSION_MINOR 0
 
-typedef struct __packed config_header
+#define SIZE_READ 100
+#define SIZE_HEADER 10
+
+#define OFFSET_MAGIC 0
+#define OFFSET_MAJOR_VERSION 2
+#define OFFSET_MINOR_VERSION 3
+#define OFFSET_DATA_LENGTH 4
+#define OFFSET_CHECKSUM 6
+
+typedef struct __packed config_header_t
 {
     uint16_t magic;
     uint8_t major_version;
     uint8_t minor_version;
     uint16_t data_length;
+    uint32_t checksum;
 };
 
+static uint8_t buffer[SIZE_READ];
+static struct config_header_t *header = (struct config_header_t *)buffer;
+
 static struct nvs_fs fs;
-static uint8_t buffer[READ_LENGTH];
 
 static int flash_init();
 static bool read_data();
 static bool check_data();
+static bool check_magic();
 static bool write_defaults();
 static bool check_crc();
 
@@ -52,7 +67,7 @@ int config_init()
     if (check_data() == 0)
     {
         LOG_INF("Configuration read and verified");
-    }   
+    }
     else
     {
         if (write_defaults() != 0)
@@ -61,7 +76,7 @@ int config_init()
             return -3;
         }
     }
-    
+
     return 0;
 }
 
@@ -91,7 +106,7 @@ static int flash_init()
 
 static bool read_data()
 {
-    if (nvs_read(&fs, CONFIG_ID, &buffer, READ_LENGTH) == 0)
+    if (nvs_read(&fs, CONFIG_ID, &buffer, SIZE_READ) == 0)
     {
         return false;
     }
@@ -101,30 +116,69 @@ static bool read_data()
 
 static bool check_data()
 {
-    if (check_magic()/*chick size*/ )
+    if (!check_magic() || header->data_length >= (SIZE_READ - SIZE_HEADER))
     {
+        LOG_ERR("Magic not found");
         return check_crc();
     }
 
-
-
-
-    if (check_magic() && cfgHeader->tlvSize < (NUMBER_OF_BYTES_READ - SIZE_HEADER -SIZE_TAIL)) {
-        return check_crc();
-    }
-    printf("CONFIG\t: EEPROM magic not found!\r\n");
     return false;
+}
 
-    return 0;
+static bool check_magic()
+{
+    return header->magic == MAGIC;
 }
 
 static bool write_defaults()
 {
-    return 0;
+    buffer[OFFSET_MAGIC] = MAGIC;
+    buffer[OFFSET_MAJOR_VERSION] = VERSION_MAJOR;
+    buffer[OFFSET_MINOR_VERSION] = VERSION_MINOR;
+    buffer[OFFSET_CHECKSUM] = 0;
 }
+
+
+// static bool write_defaults(void) {
+//   uint8_t default_anchor_list[] = {1, 2, 3, 4, 5, 6};
+
+//   buffer[0] = MAGIC;
+//   buffer[1] = 1; // Major version
+//   buffer[2] = 0; // Minor version
+//   buffer[3] = 0; // Length of TLV
+//   buffer[4] = 0; // Length of TLV
+//   buffer[5] = buffer[0] + buffer[1];
+//   // Write the default address
+//   cfgWriteU8(cfgAddress, 0);
+//   cfgWriteU8(cfgMode, MODE_ANCHOR);
+//   cfgWriteU8list(cfgAnchorlist, default_anchor_list, sizeof(default_anchor_list));
+//   write_crc();
+//   if (!eepromWrite(0, buffer, 7))
+//     return false;
+//   HAL_Delay(10);
+//   if (readData()) {
+//     if (check_content()) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   } else {
+//     return false;
+//   }
+// }
 
 bool check_crc()
 {
-    crc32_ieee(&buffer, READ_LENGTH);
-    
+    const int total_length = SIZE_HEADER + header->data_length;
+    uint32_t buffer_checksum;
+    memcpy(&buffer_checksum, &buffer[OFFSET_CHECKSUM], sizeof(uint32_t));
+    uint32_t checksum = crc32_ieee(&buffer, total_length);
+
+    if (buffer_checksum != checksum)
+    {
+        LOG_ERR("Checksums do not match");
+        return false;
+    }
+
+    return true;
 }
--- src/main.c
+++ src/main.c
@@ -4,16 +4,17 @@
 
 #include <zephyr/logging/log.h>
 #include <zephyr/shell/shell.h>
+#include <zephyr/sys/crc.h>
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 
-static int mode_cmd_handler(const struct shell *sh,
+static int algorithm_cmd_handler(const struct shell *sh,
                                  size_t argc,
                                  char **argv,
                                  void *data);
 
 SHELL_SUBCMD_DICT_SET_CREATE(sub_mode,
-                                mode_cmd_handler,
+                                algorithm_cmd_handler,
                                 (tag, TAG, "Switch to 'tag' mode"),
                                 (anchor, ANCHOR, "Switch to 'anchor' mode"));
 
@@ -29,18 +30,11 @@ int main(void)
         LOG_ERR("Failed to initialze config");
         return -1;
     }
-    LOG_DBG("Initialized config");
-
-    if (config_read(&config) != 0)
-    {
-        LOG_ERR("Failed to read config");
-        return -2;
-    }
 
     if (uwb_init(&config) != 0)
     {
         LOG_ERR("Failed to initalize uwb");
-        return -3;
+        return -2;
     }
     LOG_DBG("Initialized uwb");
 
@@ -49,8 +43,7 @@ int main(void)
     return 0;
 }
 
-int algorithm_cmd_handler(const struct shell *sh, size_t argc, char **argv, void *data)
+static int algorithm_cmd_handler(const struct shell *sh, size_t argc, char **argv, void *data)
 {
-    
     return 0;
 }
